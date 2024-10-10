1df2a29eb7dfec47a3d6b91ee3eabc856ae2617c
mliner <mliner@users.noreply.github.com>
2024-10-10 13:56:20 +0200

    rebuilding lambda package (#3)
    * rebuilding lambda package
    * updating readme
    * updating precommit
    * updating terraform code based on pre-commit findings

--- variables.tf
+++ variables.tf
@@ -1,10 +1,29 @@
 variable "lambda_schedule" {
+  type    = string
   default = "cron(0 0 * * ? *)"
 }
-variable "region" {}
-variable "cw_metric_name" {}
-variable "cw_metric_namespace" {}
-variable "cw_metric_dimension" {}
-variable "name" {}
-variable "email_source_address" {}
-variable "email_target_address" {}
+
+variable "region" {
+  type = string
+}
+variable "cw_metric_name" {
+  type        = string
+  description = "CloudWatch metric name"
+}
+variable "cw_metric_namespace" {
+  type        = string
+  description = "CloudWatch metric namespace"
+}
+variable "cw_metric_dimension" {
+  type        = string
+  description = "CloudWatch metric dimension"
+}
+variable "name" {
+  type = string
+}
+variable "email_source_address" {
+  type = string
+}
+variable "email_target_address" {
+  type = string
+}
